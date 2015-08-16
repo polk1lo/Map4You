7bfcfb064df067d6a8c82c6b5b89ba883d149281
Denis Rigkov <polk1lowa@gmail.com>
2015-08-17 01:48:00 +0400

    and last init commit :)

---
+++ src/main/java/com/map4you/tools/DataObjectHelper.java
@@ -0,0 +1,4 @@
+package com.map4you.tools;
+
+public class DataObjectHelper {
+}
---
+++ src/test/java/com/map4you/tools/DataObjectHelperTest.java
@@ -0,0 +1,19 @@
+package com.map4you.tools;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by polk1lo on 17.08.2015.
+ */
+public class DataObjectHelperTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
